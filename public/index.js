400805ebf6bcdcef45c434e47af3743317bd6a1f
Zheng-Luxi <2092028826@qq.com>
2020-05-31 18:20:19 +0800

    Update index.css & index.js

--- public/index.js
+++ public/index.js
@@ -1,5 +1,7 @@
 var outerBox = document.querySelector("#outers");
 var outers = [...document.querySelectorAll("#outers > li")];
+var innerBox = document.querySelector("#inners");
+var inners = [...document.querySelectorAll("#inners > li")];
 
 outerBox.onmouseover = function(){
 	for( let i = 0 ; i < outers.length ; i ++ ){
@@ -8,7 +10,18 @@ outerBox.onmouseover = function(){
 			case 1:
 			case 2:
 			case 3:
-				outers[i].style.transform = `rotateY( ${ ( i + 1 ) * 90 }deg ) scaleX(2) scaleY(2) translateZ(200px)`
+				outers[i].style.transform = `rotateY( ${ ( i + 1 ) * 90 }deg ) scaleX(2) scaleY(2) translateZ(200px)`;
+				outers[i].style.opacity = `.7`;
+			break;
+		}
+	}
+	for( let i = 0 ; i < inners.length ; i ++ ){
+		switch( i ){
+			case 0:
+			case 1:
+			case 2:
+			case 3:
+				inners[i].style.opacity = `1`;
 			break;
 		}
 	}
@@ -22,6 +35,17 @@ outerBox.onmouseout = function(){
 			case 2:
 			case 3:
 				outers[i].style.transform = `rotateY( ${ ( i + 1 ) * 90 }deg ) translateZ(100px)`
+				outers[i].style.opacity = `.15`;
+			break;
+		}
+	}
+	for( let i = 0 ; i < inners.length ; i ++ ){
+		switch( i ){
+			case 0:
+			case 1:
+			case 2:
+			case 3:
+				inners[i].style.opacity = `.9`;
 			break;
 		}
 	}
